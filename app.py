75ec88c69aad852da488c5dc3488323519524261
Amarjeet Singh Cheema <52501760+amarjeet-cheema@users.noreply.github.com>
2020-12-05 12:42:17 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+# Importing essential libraries
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+# Load the Random Forest CLassifier model
+filename = 'default.pkl'
+classifier = pickle.load(open(filename, 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        LIMIT_BAL = int(request.form['LIMIT_BAL'])
+        SEX = int(request.form['SEX'])
+        EDUCATION = int(request.form['EDUCATION'])
+        PAY_1 = int(request.form['PAY_1'])
+        PAY_AMT1 = int(request.form['PAY_AMT1'])
+        BILL_AMT1 = float(request.form['BILL_AMT1'])
+        
+        data = np.array([[LIMIT_BAL, SEX, EDUCATION, PAY_1, PAY_AMT1, BILL_AMT1]])
+        my_prediction = classifier.predict(data)
+        
+        return render_template('result.html', prediction=my_prediction)
+
+if __name__ == '__main__':
+	app.run(debug=True)
